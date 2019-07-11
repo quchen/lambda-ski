7ca45af7ba52baccd91ade6f9524c7170ff313d5
David Luposchainsky <dluposchainsky@gmail.com>
2019-07-11 14:31:21 +0200

    l . l == 2 l

--- helloworld/hello.py
+++ helloworld/hello.py
@@ -5,50 +5,52 @@ K = lambda x: lambda _: x
 
 # Python’s parser supports only 100 levels of nesting in expressions,
 # which we hack around by floating out deeply nested subexpressions.
-hello_1 = S(K(S(K(S(K(S(K(S(K(S(K(S))))))))))))(S(K(S(K(S(K(S(K(S(K(S(K(S(K(S)))
-    )))))))))))(S(S(K(S))(S(K(S(K(S))))(S(K(S(K(S(K(S))))))(S(K(S(K(S(K(S(K(S)))
-    )))))(S(K(S(K(S(K(S(K(S(K(S))))))))))(S(K(S(K(S(K(S(K(S(K(S(K(S))))))))))))(
-    S(K(S(K(S(K(S(K(S(K(S(K(S(K(S))))))))))))))(S(S(K(S))(S(K(K))(S(K(S))(S(K(S(
-    K(S))))(S(K(S(K(K))))(S(K(S(K(S))))(S(K(S(K(S(K(S))))))(S(K(S(K(S(K(K))))))(
-    S(K(S(K(S(K(S))))))(S(K(S(K(S(K(S(K(S))))))))(S(S(K(S))(S(K(K))(S(K(S))(S(K(
-    K))(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K(S(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(
-    K(S))(S(K(K))(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K(S))(S(K(K)
-    )(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K)(K))))))))))))))))))))
-    ))))(S(K(K))(S(K(S(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K(S))(S
-    (K(K))(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K(S
-    ))(S(K(K))(S(K)(K))))))))))))))))))))))(S(K(K))(S(K(S(S(K(S))(S(K(K))(S(K(S)
-    )(S(K(K))(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(
-    K(S))(S(K(K))(S(K(S))(S(K(K))(S(K)(K))))))))))))))))))))(S(K(S(S(K(S))(S(K(K
-    ))(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K(S))(S
-    (K(K))(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K)(K))))))))))))))))))))(S(K(S(K(S(
-    S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K(S))(S(K(
-    K))(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K)(K))))))))))))))))))))(S(K(S(K(S(K(S
+hello_1 = S(K(S(K(S(K(S(K(S(K(S(K(S(K(S(K(S))))))))))))))))(S(S(K(S))(S(K(S(K(S)
+    )))(S(K(S(K(S(K(S))))))(S(K(S(K(S(K(S(K(S))))))))(S(K(S(K(S(K(S(K(S(K(S)))))
+    )))))(S(K(S(K(S(K(S(K(S(K(S(K(S))))))))))))(S(K(S(K(S(K(S(K(S(K(S(K(S(K(S)))
+    )))))))))))(S(K(S(K(S(K(S(K(S(K(S(K(S(K(S(K(S))))))))))))))))(S(S(K(S))(S(K(
+    K))(S(K(S))(S(K(S(K(S))))(S(K(S(K(S(K(S))))))(S(K(S(K(S(K(K))))))(S(K(S(K(S(
+    K(S))))))(S(K(S(K(S(K(S(K(S))))))))(S(K(S(K(S(K(S(K(K))))))))(S(K(S(K(S(K(S(
+    K(S))))))))(S(K(S(K(S(K(S(K(S(K(S))))))))))(S(S(K(S))(S(K(S(K(S))))(S(K(S(K(
+    K))))(S(K(S(K(S))))(S(K(S(K(K))))(S(K(S(K(S))))(S(K(S(K(K))))(S(K(S(K(S))))(
+    S(K(S(K(K))))(S(K(S(K(S(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K(
+    S))(S(K(K))(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K(S))(S(K(K))(
+    S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K)(K))))))))))))))))))))))))))(S(K(S(K(K))
+    ))(S(K(S(K(S(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K(S))(S(K(K))
     (S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K(S))(S(K
-    (K))(S(K(S))(S(K(K))(S(K)(K))))))))))))))))))))(S(K(S(K(S(K(K))))))(S(K(S(K(
-    S(K(S(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K(S)
-    )(S(K(K))(S(K)(K))))))))))))))))))(S(K(S(K(S(K(K))))))(S(K(S(K(S(K(S(S(K(S))
-    (S(K(K))(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K)(K)))))))))))))
-    )))(S(K(S(K(S(K(K))))))(S(K(S(K(S(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K(S))(S(
-    K(K))(S(K(S))(S(K(K))(S(K)(K))))))))))))))(S(K(S(K(K))))(S(K(S(K(S(S(K(S))(S
-    (K(K))(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K)(K))))))))))))(S(K(S(K(K))))(S(K(
-    S(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K)(K))))))))))(S(K(K))(S
-    (K(S(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K)(K))))))))(S(K(K))(S(K(S(S(K(S))(S(
-    K(K))(S(K)(K))))))(S(K(K))(S(K(S(S(K)(K))))(S(K(K))(S(K)(K))))))))))))))))))
-    )))))))))))))))))(K(S(K(S(K(S(K(S(S(K)(K))))))))(S(K(S(K(S(K(K))))))(S(S(K(S
-    ))(S(K(K))(S(K(S))(S(S(K(S))(S(K(K))(S(K)(K))))(K(S(K)(K)))))))(K(S(K(K))(S(
-    K)(K)))))))))))))))))))(K(S(K(S(K(S(K(K))))))(S(K(S(K(S(K(S(K(S(K(S(S(K)(K))
+    (K))(S(K)(K))))))))))))))))))))))))(S(K(S(K(K))))(S(K(S(S(K(S))(S(K(S(K(S)))
+    )(S(K(S(K(K))))(S(K(S(K(S))))(S(K(S(K(K))))(S(K(S(K(S))))(S(K(S(K(K))))(S(K(
+    S(K(S))))(S(K(S(K(K))))(S(K(S(K(S))))(S(K(S(K(K))))(S(K(S(K(S))))(S(K(S(K(K)
+    )))(S(K(S(K(S))))(S(K(S(K(K))))(S(K(S(K(S))))(S(K(S(K(K))))(S(S(K(S))(S(K(K)
+    )(S(K)(K))))(K(S(K)(K)))))))))))))))))))))))(S(K(K))(S(K(S(K(S(S(K(S))(S(K(K
+    ))(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K(S))(S
+    (K(K))(S(K(S))(S(K(K))(S(K)(K))))))))))))))))))))(S(K(S(K(S(K(S(S(K(S))(S(K(
+    K))(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K(S))(
+    S(K(K))(S(K)(K))))))))))))))))))))(S(K(S(K(S(K(K))))))(S(K(S(K(S(K(S(S(K(S))
+    (S(K(K))(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K
+    )(K))))))))))))))))))(S(K(S(K(S(K(K))))))(S(K(S(K(S(K(S(S(K(S))(S(K(K))(S(K(
+    S))(S(K(K))(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K)(K))))))))))))))))(S(K(S(K(S
+    (K(K))))))(S(K(S(K(S(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K(S))
+    (S(K(K))(S(K)(K))))))))))))))(S(K(S(K(K))))(S(K(S(K(S(S(K(S))(S(K(K))(S(K(S)
+    )(S(K(K))(S(K(S))(S(K(K))(S(K)(K))))))))))))(S(K(S(K(K))))(S(K(S(S(K(S))(S(K
+    (K))(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K)(K))))))))))(S(K(K))(S(K(S(S(K(S))(
+    S(K(K))(S(K(S))(S(K(K))(S(K)(K))))))))(S(K(K))(S(K(S(S(K(S))(S(K(K))(S(K)(K)
+    )))))(S(K(K))(S(K(S(S(K)(K))))(S(K(K))(S(K)(K)))))))))))))))))))))))))))))))
+    )))))(K(K(S(K(S(K(S(K(S(S(K)(K))))))))(S(K(S(K(S(K(K))))))(S(S(K(S))(S(K(K))
+    (S(K(S))(S(S(K(S))(S(K(K))(S(K)(K))))(K(S(K)(K)))))))(K(S(K(K))(S(K)(K))))))
+    )))))))))))))))(K(S(K(K))(S(K(S(K(S(K(K))))))(S(K(S(K(S(K(S(K(S(K(S(S(K)(K))
     ))))))))))(S(K(S(K(S(K(S(K(S(K(K))))))))))(S(S(K(S))(S(K(S(K(S))))(S(K(S(K(K
     ))))(S(K(S(K(S))))(S(K(S(K(K))))(S(K(S(K(S))))(S(K(S(K(K))))(S(K(S(S(K)(K)))
     )(S(K(K))(S(K)(K)))))))))))(K(S(S(K(S))(S(K(S(K(S))))(S(K(S(K(K))))(S(K(S(K(
     S))))(S(K(S(K(K))))(S(S(K(S))(S(K(K))(S(K)(K))))(K(S(K)(K)))))))))(S(K(K))(S
     (S(K(S))(S(K(S(K(S))))(S(K(S(K(K))))(S(S(K(S))(S(K(K))(S(K)(K))))(K(S(K)(K))
-    )))))(K(K(S(K)(K)))))))))))))))))))))(K(K(S(K(S(K(S(K(S(K(S(K(S(S(K)(K))))))
-    ))))))(S(K(S(K(S(K(S(K(S(K(K))))))))))(S(S(K(S))(S(K(S(K(S))))(S(K(S(K(K))))
-    (S(K(S(K(S))))(S(K(S(K(K))))(S(K(S(K(S))))(S(K(S(K(K))))(S(S(K(S))(S(K(K))(S
-    (K)(K))))(K(S(K)(K)))))))))))(S(K(K))(S(S(K(S))(S(K(S(K(S))))(S(K(S(K(K))))(
-    S(K(S(K(S))))(S(K(S(K(K))))(S(S(K(S))(S(K(K))(S(K)(K))))(K(S(K)(K)))))))))(S
-    (K(K))(S(S(K(S))(S(K(S(K(S))))(S(K(S(K(K))))(S(S(K(S))(S(K(K))(S(K)(K))))(K(
-    S(K)(K)))))))(K(K(S(K)(K)))))))))))))))
+    )))))(K(K(S(K)(K)))))))))))))))))))))))(K(K(K(S(K(S(K(S(K(S(K(S(K(S(S(K)(K))
+    ))))))))))(S(K(S(K(S(K(S(K(S(K(K))))))))))(S(S(K(S))(S(K(S(K(S))))(S(K(S(K(K
+    ))))(S(K(S(K(S))))(S(K(S(K(K))))(S(K(S(K(S))))(S(K(S(K(K))))(S(S(K(S))(S(K(K
+    ))(S(K)(K))))(K(S(K)(K)))))))))))(S(K(K))(S(S(K(S))(S(K(S(K(S))))(S(K(S(K(K)
+    )))(S(K(S(K(S))))(S(K(S(K(K))))(S(S(K(S))(S(K(K))(S(K)(K))))(K(S(K)(K)))))))
+    ))(S(K(K))(S(S(K(S))(S(K(S(K(S))))(S(K(S(K(K))))(S(S(K(S))(S(K(K))(S(K)(K)))
+    )(K(S(K)(K)))))))(K(K(S(K)(K)))))))))))))))
 hello_0 = S(K(S(K(S))))(S(K(S(K(S(K(S))))))(S(K(S(K(S(K(S(K(S))))))))(S(K(S(K(S(
     K(S(K(S(K(S))))))))))(S(K(S(K(S(K(S(K(S(K(S(K(S))))))))))))(S(K(S(K(S(K(S(K(
     S(K(S(K(S(K(S))))))))))))))(S(K(S(K(S(K(S(K(S(K(S(K(S(K(S(K(S)))))))))))))))
@@ -71,47 +73,47 @@ hello_0 = S(K(S(K(S))))(S(K(S(K(S(K(S))))))(S(K(S(K(S(K(S(K(S))))))))(S(K(S(K(S(
     K(S))))))(S(K(S(K(S(K(S(K(S))))))))(S(K(S(K(S(K(S(K(S(K(S))))))))))(S(K(S(K(
     S(K(S(K(S(K(S(K(S))))))))))))(S(K(S(K(S(K(S(K(S(K(S(K(S(K(S))))))))))))))(S(
     K(S(K(S(K(S(K(S(K(S(K(S(K(S(K(S))))))))))))))))(S(S(K(S))(S(K(S(K(S))))(S(K(
-    S(K(K))))(S(K(S(K(S))))(S(K(S(K(S(K(S))))))(S(K(S(K(S(K(S(K(S))))))))(S(K(S(
-    K(S(K(S(K(S(K(S))))))))))(hello_1))))))))(K(S(K(S(K(S(K(S(K(S(K(S(K(S(K(S(S(
-    K)(K))))))))))))))))(S(K(S(K(S(K(S(K(S(K(S(K(S(K(K))))))))))))))(S(S(K(S))(S
-    (K(K))(S(K(S))(S(K(S(K(S))))(S(K(S(K(K))))(S(K(S(K(S))))(S(K(S(K(K))))(S(K(S
-    (K(S))))(S(K(S(K(K))))(S(K(S(K(S))))(S(K(S(K(K))))(S(K(S(S(K)(K))))(S(K(K))(
-    S(K)(K)))))))))))))))(K(S(S(K(S))(S(K(S(K(S))))(S(K(S(K(K))))(S(K(S(K(S))))(
-    S(K(S(K(K))))(S(K(S(K(S))))(S(K(S(K(K))))(S(K(S(K(S))))(S(K(S(K(K))))(S(K(S(
-    S(K)(K))))(S(K(K))(S(K)(K)))))))))))))(K(S(S(K(S))(S(K(S(K(S))))(S(K(S(K(K))
-    ))(S(K(S(K(S))))(S(K(S(K(K))))(S(K(S(K(S))))(S(K(S(K(K))))(S(S(K(S))(S(K(K))
-    (S(K)(K))))(K(S(K)(K)))))))))))(S(K(K))(S(S(K(S))(S(K(S(K(S))))(S(K(S(K(K)))
-    )(S(K(S(K(S))))(S(K(S(K(K))))(S(S(K(S))(S(K(K))(S(K)(K))))(K(S(K)(K)))))))))
-    (S(K(K))(S(S(K(S))(S(K(S(K(S))))(S(K(S(K(K))))(S(S(K(S))(S(K(K))(S(K)(K))))(
-    K(S(K)(K)))))))(K(K(S(K)(K)))))))))))))))))))))))))(K(K(K(S(K(S(K(S(K(S(K(K)
-    )))))))(S(K(S(K(S(K(S(K(S(S(K)(K))))))))))(S(K(S(K(S(K(S(K(K))))))))(S(S(K(S
-    ))(S(K(S(K(S))))(S(K(S(K(K))))(S(K(S(K(S))))(S(K(S(K(K))))(S(S(K(S))(S(K(K))
-    (S(K)(K))))(K(S(K)(K)))))))))(S(K(K))(S(S(K(S))(S(K(S(K(S))))(S(K(S(K(K))))(
-    S(S(K(S))(S(K(K))(S(K)(K))))(K(S(K)(K)))))))(K(K(S(K)(K)))))))))))))))))))))
-    )(K(K(K(K(K(K(S(K(K))(S(K(S(S(K)(K))))(S(K(K))(S(K)(K))))))))))))))))))))))(
-    K(S(K(S(K(S(K(S(K(K))))))))(S(K(S(K(S(K(S(K(S(K(S(K(S(K(S(S(K)(K))))))))))))
-    ))))(S(K(S(K(S(K(S(K(S(K(S(K(S(K(K))))))))))))))(S(S(K(S))(S(K(K))(S(K(S))(S
-    (K(S(K(S))))(S(K(S(K(K))))(S(K(S(K(S))))(S(K(S(K(K))))(S(K(S(K(S))))(S(K(S(K
-    (K))))(S(K(S(K(S))))(S(K(S(K(K))))(S(K(S(S(K)(K))))(S(K(K))(S(K)(K))))))))))
-    )))))(K(S(S(K(S))(S(K(S(K(S))))(S(K(S(K(K))))(S(K(S(K(S))))(S(K(S(K(K))))(S(
-    K(S(K(S))))(S(K(S(K(K))))(S(K(S(K(S))))(S(K(S(K(K))))(S(K(S(S(K)(K))))(S(K(K
-    ))(S(K)(K)))))))))))))(K(S(S(K(S))(S(K(S(K(S))))(S(K(S(K(K))))(S(K(S(K(S))))
-    (S(K(S(K(K))))(S(K(S(K(S))))(S(K(S(K(K))))(S(S(K(S))(S(K(K))(S(K)(K))))(K(S(
-    K)(K)))))))))))(S(K(K))(S(S(K(S))(S(K(S(K(S))))(S(K(S(K(K))))(S(K(S(K(S))))(
-    S(K(S(K(K))))(S(S(K(S))(S(K(K))(S(K)(K))))(K(S(K)(K)))))))))(S(K(K))(S(S(K(S
-    ))(S(K(S(K(S))))(S(K(S(K(K))))(S(S(K(S))(S(K(K))(S(K)(K))))(K(S(K)(K)))))))(
-    K(K(S(K)(K)))))))))))))))))))))))))))(K(K(S(K(S(K(K))))(S(K(S(K(K))))(S(K(S(
-    K(S(K(S(K(S(K(S(S(K)(K))))))))))))(S(K(S(K(S(K(S(K(S(K(K))))))))))(S(S(K(S))
-    (S(K(S(K(S))))(S(K(S(K(K))))(S(K(S(K(S))))(S(K(S(K(K))))(S(K(S(K(S))))(S(K(S
-    (K(K))))(S(K(S(S(K)(K))))(S(K(K))(S(K)(K)))))))))))(K(S(S(K(S))(S(K(S(K(S)))
-    )(S(K(S(K(K))))(S(K(S(K(S))))(S(K(S(K(K))))(S(S(K(S))(S(K(K))(S(K)(K))))(K(S
-    (K)(K)))))))))(S(K(K))(S(S(K(S))(S(K(S(K(S))))(S(K(S(K(K))))(S(S(K(S))(S(K(K
-    ))(S(K)(K))))(K(S(K)(K)))))))(K(K(S(K)(K)))))))))))))))))))))))))(K(K(K(S(K(
-    S(K(K))))(S(K(S(K(K))))(S(K(S(K(S(K(S(K(S(S(K)(K))))))))))(S(K(S(K(S(K(S(K(K
-    ))))))))(S(S(K(S))(S(K(S(K(S))))(S(K(S(K(K))))(S(K(S(K(S))))(S(K(S(K(K))))(S
-    (S(K(S))(S(K(K))(S(K)(K))))(K(S(K)(K)))))))))(S(K(K))(S(S(K(S))(S(K(S(K(S)))
-    )(S(K(S(K(K))))(S(S(K(S))(S(K(K))(S(K)(K))))(K(S(K)(K)))))))(K(K(S(K)(K)))))
-    )))))))))))))))))
+    S(K(S(K(S))))))(S(K(S(K(S(K(S(K(S))))))))(S(K(S(K(S(K(S(K(S(K(S))))))))))(S(
+    K(S(K(S(K(S(K(S(K(S(K(S))))))))))))(S(K(S(K(S(K(S(K(S(K(S(K(S(K(S)))))))))))
+    )))(hello_1))))))))(K(S(K(S(K(S(K(S(K(S(K(S(K(S(K(S(S(K)(K))))))))))))))))(S
+    (K(S(K(S(K(S(K(S(K(S(K(S(K(K))))))))))))))(S(S(K(S))(S(K(K))(S(K(S))(S(K(S(K
+    (S))))(S(K(S(K(K))))(S(K(S(K(S))))(S(K(S(K(K))))(S(K(S(K(S))))(S(K(S(K(K))))
+    (S(K(S(K(S))))(S(K(S(K(K))))(S(K(S(S(K)(K))))(S(K(K))(S(K)(K)))))))))))))))(
+    K(S(S(K(S))(S(K(S(K(S))))(S(K(S(K(K))))(S(K(S(K(S))))(S(K(S(K(K))))(S(K(S(K(
+    S))))(S(K(S(K(K))))(S(K(S(K(S))))(S(K(S(K(K))))(S(K(S(S(K)(K))))(S(K(K))(S(K
+    )(K)))))))))))))(K(S(S(K(S))(S(K(S(K(S))))(S(K(S(K(K))))(S(K(S(K(S))))(S(K(S
+    (K(K))))(S(K(S(K(S))))(S(K(S(K(K))))(S(S(K(S))(S(K(K))(S(K)(K))))(K(S(K)(K))
+    )))))))))(S(K(K))(S(S(K(S))(S(K(S(K(S))))(S(K(S(K(K))))(S(K(S(K(S))))(S(K(S(
+    K(K))))(S(S(K(S))(S(K(K))(S(K)(K))))(K(S(K)(K)))))))))(S(K(K))(S(S(K(S))(S(K
+    (S(K(S))))(S(K(S(K(K))))(S(S(K(S))(S(K(K))(S(K)(K))))(K(S(K)(K)))))))(K(K(S(
+    K)(K)))))))))))))))))))))))))(K(K(K(S(K(S(K(S(K(S(K(K))))))))(S(K(S(K(S(K(S(
+    K(S(S(K)(K))))))))))(S(K(S(K(S(K(S(K(K))))))))(S(S(K(S))(S(K(S(K(S))))(S(K(S
+    (K(K))))(S(K(S(K(S))))(S(K(S(K(K))))(S(S(K(S))(S(K(K))(S(K)(K))))(K(S(K)(K))
+    )))))))(S(K(K))(S(S(K(S))(S(K(S(K(S))))(S(K(S(K(K))))(S(S(K(S))(S(K(K))(S(K)
+    (K))))(K(S(K)(K)))))))(K(K(S(K)(K))))))))))))))))))))))(K(K(K(K(K(K(S(K(K))(
+    S(K(S(S(K)(K))))(S(K(K))(S(K)(K))))))))))))))))))))))(K(S(K(S(K(S(K(S(K(K)))
+    )))))(S(K(S(K(S(K(S(K(S(K(S(K(S(K(S(S(K)(K))))))))))))))))(S(K(S(K(S(K(S(K(S
+    (K(S(K(S(K(K))))))))))))))(S(S(K(S))(S(K(K))(S(K(S))(S(K(S(K(S))))(S(K(S(K(K
+    ))))(S(K(S(K(S))))(S(K(S(K(K))))(S(K(S(K(S))))(S(K(S(K(K))))(S(K(S(K(S))))(S
+    (K(S(K(K))))(S(K(S(S(K)(K))))(S(K(K))(S(K)(K)))))))))))))))(K(S(S(K(S))(S(K(
+    S(K(S))))(S(K(S(K(K))))(S(K(S(K(S))))(S(K(S(K(K))))(S(K(S(K(S))))(S(K(S(K(K)
+    )))(S(K(S(K(S))))(S(K(S(K(K))))(S(K(S(S(K)(K))))(S(K(K))(S(K)(K)))))))))))))
+    (K(S(S(K(S))(S(K(S(K(S))))(S(K(S(K(K))))(S(K(S(K(S))))(S(K(S(K(K))))(S(K(S(K
+    (S))))(S(K(S(K(K))))(S(S(K(S))(S(K(K))(S(K)(K))))(K(S(K)(K)))))))))))(S(K(K)
+    )(S(S(K(S))(S(K(S(K(S))))(S(K(S(K(K))))(S(K(S(K(S))))(S(K(S(K(K))))(S(S(K(S)
+    )(S(K(K))(S(K)(K))))(K(S(K)(K)))))))))(S(K(K))(S(S(K(S))(S(K(S(K(S))))(S(K(S
+    (K(K))))(S(S(K(S))(S(K(K))(S(K)(K))))(K(S(K)(K)))))))(K(K(S(K)(K))))))))))))
+    )))))))))))))))(K(K(S(K(S(K(K))))(S(K(S(K(K))))(S(K(S(K(S(K(S(K(S(K(S(S(K)(K
+    ))))))))))))(S(K(S(K(S(K(S(K(S(K(K))))))))))(S(S(K(S))(S(K(S(K(S))))(S(K(S(K
+    (K))))(S(K(S(K(S))))(S(K(S(K(K))))(S(K(S(K(S))))(S(K(S(K(K))))(S(K(S(S(K)(K)
+    )))(S(K(K))(S(K)(K)))))))))))(K(S(S(K(S))(S(K(S(K(S))))(S(K(S(K(K))))(S(K(S(
+    K(S))))(S(K(S(K(K))))(S(S(K(S))(S(K(K))(S(K)(K))))(K(S(K)(K)))))))))(S(K(K))
+    (S(S(K(S))(S(K(S(K(S))))(S(K(S(K(K))))(S(S(K(S))(S(K(K))(S(K)(K))))(K(S(K)(K
+    )))))))(K(K(S(K)(K)))))))))))))))))))))))))(K(K(K(S(K(S(K(K))))(S(K(S(K(K)))
+    )(S(K(S(K(S(K(S(K(S(S(K)(K))))))))))(S(K(S(K(S(K(S(K(K))))))))(S(S(K(S))(S(K
+    (S(K(S))))(S(K(S(K(K))))(S(K(S(K(S))))(S(K(S(K(K))))(S(S(K(S))(S(K(K))(S(K)(
+    K))))(K(S(K)(K)))))))))(S(K(K))(S(S(K(S))(S(K(S(K(S))))(S(K(S(K(K))))(S(S(K(
+    S))(S(K(K))(S(K)(K))))(K(S(K)(K)))))))(K(K(S(K)(K))))))))))))))))))))))
 hello = S(S(K(S))(S(K(S(K(S))))(S(K(S(K(S(K(S))))))(S(S(K(S))(S(K(S(K(S))))(S(K(
     S(K(S(K(S))))))(S(S(K(S))(S(K(S(K(S))))(S(K(S(K(S(K(S))))))(S(S(K(S))(S(K(S(
     K(S))))(S(K(S(K(S(K(S))))))(S(K(S(S(K(S))(S(K(K))(S(K(S))(S(K(K))(S(K(S))(S(
